49de8ea5ef638eb0f0c2e5010ca60627f235ec47
AdrianAguinaga <adrian.aguinaga@uabc.edu.mx>
2026-03-02 19:41:47 -0800

    Refactor saveTask and update functions to enhance AJAX handling and improve task display logic

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,6 @@ function hello()
 // An example of changing the logic exec
 function saveTask(){
     console.log("Saving Task");
-
     // get the values from the form
     const title = $("#txtTitle").val();
     const desc = $("#txtDescription").val();
@@ -16,8 +15,44 @@ function saveTask(){
     // create a new task object
     const task = new Task(title,desc,color,date,status,budget);
     console.log(task);
-    displayTask(task);    
+    //Send to server
+    
+    $.ajax({
+        type:"POST",//HTTP Verb: Create
+        url:API,
+        data:JSON.stringify(task),
+        contentType:"application/json",
+        success: function(created){
+            displayTask(task); 
+            console.log(created);
+        },
+        error: function(err){
+            console.log(err);
+        }
+     })  
 }
+
+// minichallenge 
+// use put method to update one of the existing entrys
+// tip: you must use the ID - url: API/# -- 
+// https://106api-b0bnggbsgnezbzcz.westus3-01.azurewebsites.net/api/tasks/#
+// modify the entry with the ID=1, using a title that says "Hello my name is - your name"
+
+function update(){
+    $.ajax({
+        type:"PUT",// PUT verb: UPDATE
+        url: API + "/1",
+        data: JSON.stringify({title:"Hello my name is Adrian"}),
+        contentType:"application/json",
+        success: function(response){
+            console.log(response);
+        },
+        error: function(err){
+            console.log(err);
+        }
+    })
+}
+
 
 function displayTask(task){
     let syntax = `
@@ -46,6 +81,7 @@ function loadTasks(){
     $.ajax({
         type:"GET",// HTTP method - READ
         url: API,
+        dataType: "json",//Expected formal
         success: function(data){
             console.log("Data received", data);
         },
@@ -54,6 +90,7 @@ function loadTasks(){
         }
     })
 }
+
 
 
 function init ()
